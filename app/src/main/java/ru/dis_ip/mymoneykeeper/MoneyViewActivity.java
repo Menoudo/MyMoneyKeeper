10a4cf8860a57da610224a021c703abd2389e7ad
Menoudo <priahin@list.ru>
2017-12-04 20:56:27 +0500

    * Update 1

--- app/src/main/java/ru/dis_ip/mymoneykeeper/MoneyViewActivity.java
+++ app/src/main/java/ru/dis_ip/mymoneykeeper/MoneyViewActivity.java
@@ -1,5 +1,7 @@
 package ru.dis_ip.mymoneykeeper;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +12,16 @@ public class MoneyViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.moneyview);
     }
+
+    @Override
+    protected  void onStart() {
+        super.onStart();
+        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (wifiManager != null) {
+            wifiManager.setWifiEnabled(true);
+            //wifiManager.setWifiEnabled(false);
+        }
+    }
+
 }
